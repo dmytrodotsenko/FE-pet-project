39cd85c894ea497a6f54f4c4e36039436668386d
dmytrodotsenko <dmytrodotsenko97@gmail.com>
2022-10-18 13:35:53 +0300

    chat refactor

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ChatBox } from "react-chatbox-component";
 import "react-chatbox-component/dist/style.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,14 +8,16 @@ import { getMessanges } from "../../store/chat/chatActions";
 import { sendMessage } from "../../store/chat/chatActions";
 const Chat = () => {
     const {id} = useParams();
+    const [sentMessage, setSendMessage] = useState(false);
     const dispatch = useDispatch();
     const {messanges} = useSelector(state => state.chat);
     console.log(messanges)
     useEffect(() => {
      dispatch(getMessanges({id: id}));
-    }, [dispatch, id])
+    }, [dispatch, id, sentMessage])
     const handleSubmit = (text) => {
         dispatch(sendMessage({id: id, text: text}))
+        window.location.href = `http://localhost:3000/chat/${id}`;
     }
 
   return (
